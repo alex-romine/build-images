faf0f0132e94b864857e17b2fc7d833635cb79de
Alex Romine <alexromine3@gmail.com>
2021-10-30 17:10:38 -0600

    this should be working

--- build-and-push-image.sh
+++ build-and-push-image.sh
@@ -1,10 +1,13 @@
 set -e
 
 IMAGE_NAME="$1"
-#IMAGE_VERSION="0.1.$(date +%Y%m%d%I%M%S)"
-IMAGE_VERSION="latest"
+IMAGE_VERSION="0.1.$(date +%Y%m%d%I%M%S)"
 
 echo 'building image'
 cd "$IMAGE_NAME"
+
+docker build . -t "aromine2/$IMAGE_NAME:latest"
+docker push "aromine2/$IMAGE_NAME:latest"
+
 docker build . -t "aromine2/$IMAGE_NAME:$IMAGE_VERSION"
 docker push "aromine2/$IMAGE_NAME:$IMAGE_VERSION"
